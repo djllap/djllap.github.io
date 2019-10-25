3651b15e896bdb79fb8c29ac9112aafaf4da39dd
Daniel Kent <Kentpercussion@gmail.com>
2019-10-25 15:35:33 -0600

    end of day Friday

--- scripts.js
+++ scripts.js
@@ -1,12 +1,13 @@
+'use strict';
 
 function handleStartQuiz() {
-    // Begins the Quiz -> Change View to Question 1
+  // Begins the Quiz -> Change View to Question 1
 }
 
 function handleQuestionSubmit() {
-    // Check to see if correct
-    // Update information
-    // Navigate to confirmation page
+  // Check to see if correct
+  // Update information
+  // Navigate to confirmation page
 }
 
 // Rendering
@@ -15,47 +16,76 @@ function handleQuestionSubmit() {
 // function renderFeedbackPage(bool: )
 
 function renderFeedback(question) {
-    // check right/wrong -> display page
+  // check right/wrong -> display page
 
-    // // Change submit button to a 'next page' button
-    // // Display confirmation text
+  // // Change submit button to a 'next page' button
+  // // Display confirmation text
 }
 
-function renderQuestionTitle() {
-    // Renders the 'Title ${questionNumber}'
-}
+// function renderQuestionTitle(question) {
+//   // Renders the 'Title ${questionNumber}'
+//   return `<h2>${question.title}</h2>`;
 
-function renderQuestionText() {
-    // Renders the text of the question
+// }
+
+function renderQuestionText(question) {
+  // Renders the text of the question
+  return `<p class="question">${question.text}</p>`;
 }
 
 function renderRadio() {
-    // Renders the Radio Buttons
+  // Renders the Radio Buttons
 }
 
 function renderQuestionOptions() {
-    renderRadio();
+  renderRadio();
 }
 
 function renderStats() {
-    // Renders the stats: - question number + correct/incorrect
+  // Renders the stats: - question number + correct/incorrect
 }
 
+
 function renderQuestionBox() {
-    // Renders the Question Box content
-    renderStats();
-    renderQuestionTitle();
-    renderQuestionText();
-    renderQuestionOptions();
-    renderFeedback();
+  // Renders the Question Box content
+  let index = 0;
+  let question = STORE.quizObj.questions[index];
+  $('.container-tile').html(`
+    <p class="stats">
+      <span>current question</span>
+      <span>current score</span>
+    </p>
+    <h2>${question.title}</h2>
+    <div class="question-container">
+      <p class="question">
+        ${question.text}
+      </p>
+      <form action="submit">
+        <input type="radio" name="choice1" id="putid1here">choice 1<br>
+        <input type="radio" name="choice2" id="putid2here">choice 2<br>
+        <input type="radio" name="choice3" id="putid3here">choice 3<br>
+        <input type="radio" name="choice4" id="putid4here">choice 4<br>
+        <button class="submit-button" type="submit">CLICK ME!</button>
+      </form>
+    </div>
+  `);
 }
 
 function testHTML() {
-
+  $('.container-tile').html(
+    renderQuestionTitle(STORE.quizObj.questions[0]) +
+    renderQuestionText(STORE.quizObj.questions[0])
+  );
 }
 
 function test() {
-    $('.container-tile').html('"Hello World"');
+  $('.container-tile').html('"Hello World"');
 }
 
-// test();
+function run() {
+
+}
+
+
+
+renderQuestionBox();
--- store.js
+++ store.js
@@ -1,40 +1,42 @@
+'use strict';
+
 const STORE = {
-    quizObj: {
-        // Index '0' is the landing page.
-        // Index 'questions.length' is the 'done with quiz' page.
-        pageIndex: 0,
-        questions: [
-            {
-                number: 1,
-                title: 'Question 1',
-                text: 'Which is the biggest dinosaur?',
-                options: [
-                    {
-                        text: 'T-Rex',
-                        isCorrect: true,
-                    },
-                    {
-                        text: 'T-Minus',
-                        isCorrect: false,
-                    },
-                    {
-                        text: 'T-Minus-Minus',
-                        isCorrect: false,
-                    },
-                    {
-                        text: 'T-Minus-Minus-Minus',
-                        isCorrect: false,
-                    },
-                ],
-            },
-            {
+  quizObj: {
+    // Index '0' is the landing page.
+    // Index 'questions.length' is the 'done with quiz' page.
+    pageIndex: 0,
+    questions: [
+      {
+        number: 1,
+        title: 'Question 1',
+        text: 'Which is the biggest dinosaur?',
+        options: [
+          {
+            text: 'T-Rex',
+            isCorrect: true,
+          },
+          {
+            text: 'T-Minus',
+            isCorrect: false,
+          },
+          {
+            text: 'T-Minus-Minus',
+            isCorrect: false,
+          },
+          {
+            text: 'T-Minus-Minus-Minus',
+            isCorrect: false,
+          },
+        ],
+      },
+      {
 
-            },
-            {
+      },
+      {
 
-            }
-        ]
-    }    
+      }
+    ]
+  }    
 }
 
 // let option1 = ["sdfds", true,];
