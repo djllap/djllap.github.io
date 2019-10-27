f5c6a0c34e7dc9a46d7fabef1467106b5dce9913
grobelDev <grobeldev@gmail.com>
2019-10-27 11:34:41 -0700

    implemented quiz back-end logic

--- scripts.js
+++ scripts.js
@@ -1,91 +1,253 @@
 'use strict';
 
-function handleStartQuiz() {
-  // Begins the Quiz -> Change View to Question 1
+// TERMINOLOGY:
+// 'Stats and Header' is the top of the question box
+// 'Question Container' holds the question itself
+
+
+// SECTION: - Generating HTML Code
+
+// Initialize all containers (Run once at the beginning)
+function generateInitQuestionBox() {
+  return `
+    <div class="stats-and-header">
+      <p class="stats">
+          <span></span>
+          <span></span>
+      </p>
+      <h2></h2>
+    </div>
+    <div class="question-container">
+    </div>
+    <div class="feedback-container">
+    </div>
+    <div class="landing-container">
+    </div>
+    <div class="results-container">
+    </div>
+  `;
 }
 
-function handleQuestionSubmit() {
-  // Check to see if correct
-  // Update information
-  // Navigate to confirmation page
+// pageIndex: -1
+function generateLandingContainer() {
+  return  `
+    <h2>BEGIN QUIZ</h2>
+    <form id="js-landing-form">
+      <button class="submit-button" type="submit">BEGIN QUIZ</button>
+    </form>
+  `
 }
 
-// Rendering
-// CORRECT PAGE - INCORRECT PAGE 
+// pageIndex: 0 to questions.length - 1
+function generateStatsAndHeader() {
+  let currentQuestion = STORE.quizObj.pageIndex + 1;
+  let totalQuestions = STORE.quizObj.questions.length;
+  let score = STORE.quizObj.score;
 
-// function renderFeedbackPage(bool: )
-
-function renderFeedback(question) {
-  // check right/wrong -> display page
-
-  // // Change submit button to a 'next page' button
-  // // Display confirmation text
+  return `
+    <p class="stats">
+      <span>Question: ${currentQuestion}/${totalQuestions}</span>
+      <span>Score: ${score}</span>
+    </p>
+  `;
 }
 
-// function renderQuestionTitle(question) {
-//   // Renders the 'Title ${questionNumber}'
-//   return `<h2>${question.title}</h2>`;
-
-// }
-
-function renderQuestionText(question) {
-  // Renders the text of the question
-  return `<p class="question">${question.text}</p>`;
+function generateQuestionContainer(question) {
+  return `
+    <h2>${question.title}</h2>
+    <p class="question">
+      ${question.text}
+    </p>
+    <form action="submit" id="js-question-box-form">
+      <input type="radio" name="choice" value="0">${question.options[0].text}<br>
+      <input type="radio" name="choice" value="1">${question.options[1].text}<br>
+      <input type="radio" name="choice" value="2">${question.options[2].text}<br>
+      <input type="radio" name="choice" value="3">${question.options[3].text}<br>
+      <button class="submit-button" type="submit">CLICK ME!</button>
+    </form>
+  `;
 }
 
-function renderRadio() {
-  // Renders the Radio Buttons
+function generateFeedbackContainer(feedbackObj) { 
+  return `
+    <h2>${feedbackObj.title}</h2>
+    <p>${feedbackObj.text}</p>
+    <form action="submit" id="js-feedback-box-form">
+      <button class="submit-button" type="submit">Next Question</button>
+    </form>
+  `;
 }
 
-function renderQuestionOptions() {
-  renderRadio();
+// pageIndex: questions.length
+function generateResultsContainer() {
+
+  let currentQuestion = STORE.quizObj.pageIndex + 1;
+  let totalQuestions = STORE.quizObj.questions.length;
+  let score = STORE.quizObj.score;
+
+  return `
+    <h2>Quiz Completed!</h2>
+    <div class="results">
+      <h3>Final Score: ${score}</h3>
+    </div>
+    <form>
+      <button class="submit-button" type="submit">Start Over!</button>
+    </form>
+  `;
 }
 
-function renderStats() {
-  // Renders the stats: - question number + correct/incorrect
-}
+// SECTION: - Putting the HTML into the DIVs
 
+function initQuestionBox() {
+  // Run this when generating questions.
+  // This should init the <div>'s we are working with.
+  let index = STORE.quizObj.pageIndex;
+  let question = STORE.quizObj.questions[index];
+  
+  // Init Empty Divs
+  $('.container-tile').html(generateInitQuestionBox);
+
+  // Fill the Landing <div>
+  $('.landing-container').html(generateLandingContainer());
+}
 
 function renderQuestionBox() {
   // Renders the Question Box content
-  let index = 0;
+  let index = STORE.quizObj.pageIndex;
   let question = STORE.quizObj.questions[index];
-  $('.container-tile').html(`
-    <p class="stats">
-      <span>current question</span>
-      <span>current score</span>
-    </p>
-    <h2>${question.title}</h2>
-    <div class="question-container">
-      <p class="question">
-        ${question.text}
-      </p>
-      <form action="submit">
-        <input type="radio" name="choice1" id="putid1here">choice 1<br>
-        <input type="radio" name="choice2" id="putid2here">choice 2<br>
-        <input type="radio" name="choice3" id="putid3here">choice 3<br>
-        <input type="radio" name="choice4" id="putid4here">choice 4<br>
-        <button class="submit-button" type="submit">CLICK ME!</button>
-      </form>
-    </div>
-  `);
+
+  // const statsAndHeader = generateStatsAndHeader();
+  $('.stats-and-header').html(generateStatsAndHeader());
+  $('.question-container').html(generateQuestionContainer(question));
+  $('.feedback-container').html('');
 }
 
-function testHTML() {
-  $('.container-tile').html(
-    renderQuestionTitle(STORE.quizObj.questions[0]) +
-    renderQuestionText(STORE.quizObj.questions[0])
-  );
+function renderFeedbackBox(feedbackBool) {
+  // Renders the Answer Box content
+  let index = STORE.quizObj.pageIndex;
+  let question = STORE.quizObj.questions[index];
+  let feedback = question.feedback;
+
+  if(feedbackBool) {
+    $('.feedback-container').html(generateFeedbackContainer(feedback.correct));
+  } else {
+    $('.feedback-container').html(generateFeedbackContainer(feedback.incorrect));
+  }
+  $('.question-container').html('');
 }
 
-function test() {
-  $('.container-tile').html('"Hello World"');
+function renderResultsBox() {
+  // Renders Results Content
+  $('.stats-and-header').html('');
+  $('.feedback-container').html('');
+  $('.results-container').html(generateResultsContainer());
 }
 
-function run() {
+// SECTION: - Handlers
 
+function handleLandingSubmit() {
+  $('.landing-container').on('submit', '#js-landing-form', function (event) {
+    event.preventDefault();
+    
+    $('.landing-container').html('');
+    renderQuestionBox();
+  });
+}
+
+function handleQuizSubmit() {
+  $('.question-container').on('submit', '#js-question-box-form', function (event) {
+    event.preventDefault();
+
+    let pageIndex = STORE.quizObj.pageIndex;
+    let questionOptions = STORE.quizObj.questions[pageIndex].options;
+    let correctOption = questionOptions.findIndex(option => option.isCorrect === true);
+
+    // Grab the 'checked' radio value on submit (as Int)
+    let chosenValue = Number(document.querySelector('input[name="choice"]:checked').value);
+
+    // DEBUG: - Feedback on Question
+    // alert(`correctOption: ${correctOption}, chosenValue: ${chosenValue}`);
+    // alert(correctOption === chosenValue ? 'correct!' : 'incorrect!');
+
+    if(correctOption === chosenValue) {
+      STORE.quizObj.score += 1;
+      renderFeedbackBox(true);
+    } else {
+      renderFeedbackBox(false);
+    }
+
+  });
+}
+
+function handleFeedbackSubmit() {
+  $('.feedback-container').on('submit', '#js-feedback-box-form', function (event) {
+    event.preventDefault();
+    
+    // Increment the Page
+    if(STORE.quizObj.questions.length - 1 === STORE.quizObj.pageIndex) {
+      // alert('quiz finished!');
+      renderResultsBox();
+    } else {
+      STORE.quizObj.pageIndex += 1;
+      renderQuestionBox();
+    }
+  });
 }
 
 
+// SECTION: - Main
+function handleQuestionBox() {
+  initQuestionBox();
+  handleQuizSubmit();
+  handleFeedbackSubmit();
+  handleLandingSubmit();
+}
 
-renderQuestionBox();
+$(handleQuestionBox);
+
+
+
+// // function handleStartQuiz() {
+// //   // Begins the Quiz -> Change View to Question 1
+// // }
+
+// // function handleQuestionSubmit() {
+// //   // Check to see if correct
+// //   // Update information
+// //   // Navigate to confirmation page
+// // }
+
+// // Rendering
+// // CORRECT PAGE - INCORRECT PAGE 
+
+// // function renderFeedbackPage(bool: )
+
+// function renderFeedback(question) {
+//   // check right/wrong -> display page
+
+//   // // Change submit button to a 'next page' button
+//   // // Display confirmation text
+// }
+
+// // function renderQuestionTitle(question) {
+// //   // Renders the 'Title ${questionNumber}'
+// //   return `<h2>${question.title}</h2>`;
+
+// // }
+
+// // function renderQuestionText(question) {
+// //   // Renders the text of the question
+// //   return `<p class="question">${question.text}</p>`;
+// // }
+
+// // function renderRadio() {
+// //   // Renders the Radio Buttons
+// // }
+
+// // function renderQuestionOptions() {
+// //   renderRadio();
+// // }
+
+// function renderStats() {
+//   // Renders the stats: - question number + correct/incorrect
+// }
--- store.js
+++ store.js
@@ -1,17 +1,17 @@
 'use strict';
 
 const STORE = {
+
   quizObj: {
     // Index '0' is the landing page.
     // Index 'questions.length' is the 'done with quiz' page.
     pageIndex: 0,
-
+    score: 0,
     questions: [
-      {
-        title: 'How much do you know about Dinosaurs?'
-        text: 'Everyone loves Dinosaurs, but how much do we really know about them? Take this quiz to find out if your knowledge is up to snuff.'
-      },
-
+      // {
+      //   title: 'How much do you know about Dinosaurs?'
+      //   text: 'Everyone loves Dinosaurs, but how much do we really know about them? Take this quiz to find out if your knowledge is up to snuff.'
+      // },
       {
         title: 'Question 1',
         text: 'What was the largest canivorous dinosaur?',
@@ -41,7 +41,7 @@ const STORE = {
             text: 'Wow, you really know your stuff!',
             img: ''
           },
-          incorect: {
+          incorrect: {
             title: 'WRONG',
             text: 'The largest known carnivorous dinosaur is the Spinosaurus, which could grow to be over 50 feet long and weighed in at over 20 tonnes -- three times the weight of a T-rex.',
             img: ''
@@ -78,7 +78,7 @@ const STORE = {
             text: 'Wow, did you really know that, or did you look it up?',
             img: ''
           },
-          incorect: {
+          incorrect: {
             title: 'WRONG',
             text: 'The Megalosaurus was the first dinosuar discovererd and wasn\'t found until the 1820s',
             img: ''
@@ -115,7 +115,7 @@ const STORE = {
             text: 'Don\'t let it go to your head too much.',
             img: ''
           },
-          incorect: {
+          incorrect: {
             title: 'WRONG',
             text: 'The term “dinosaur” wasn\'t coined until 1842, by English paleontologist Sir Richard Owen; "dinosaur" translates to “terrible reptile.”',
             img: ''
@@ -152,7 +152,7 @@ const STORE = {
             text: 'Are you sure you\'re not cheating? How could you possibly know that?',
             img: ''
           },
-          incorect: {
+          incorrect: {
             title: 'WRONG',
             text: 'Veins in Stegosaurus\' spinal plates suggested that they would not have been used as protective armor; more than likely, they were used to help the dinosaur cool off its massive body.',
             img: ''
@@ -189,7 +189,7 @@ const STORE = {
             text: 'Good job, I guess.',
             img: ''
           },
-          incorect: {
+          incorrect: {
             title: 'WRONG',
             text: 'The Triceratops had the largest shielded skull of any known dinosaur. With a thick bony shield that covered its neck, a Triceratops skull could grow to be as long as 6 and ½ feet long.',
             img: ''
@@ -199,35 +199,3 @@ const STORE = {
     ]
   }    
 }
-
-// let option1 = ["sdfds", true,];
-
-// function optionConstructorHelper(text, isCorrect) {
-//     return {
-//         text,
-//         isCorrect
-//     }
-// }
-
-// function optionOtherHelper(optionArray) {
-//     return optionConstructorHelper(optionArray[0], optionArray[1]);
-// }
-
-// function optionsConstructor(option_1, option_2, option_3, option_4) {
-//     return {
-//         option_1,
-//         option_2,
-//         option_3,
-//         option_4,
-//     }
-// }
-
-
-
-// function questionConstructor(title, text, options) {
-//     return {
-//         title,
-//         text,
-//         options
-//     }
-// }
