b4d25927cd5399968c84f1e1f3627f7c219e579a
Daniel Kent <Kentpercussion@gmail.com>
2019-10-27 16:28:27 -0600

    added images to data

--- store.js
+++ store.js
@@ -8,14 +8,15 @@ const STORE = {
     pageIndex: 0,
     score: 0,
     questions: [
-      // {
-      //   title: 'How much do you know about Dinosaurs?'
-      //   text: 'Everyone loves Dinosaurs, but how much do we really know about them? Take this quiz to find out if your knowledge is up to snuff.'
-      // },
+      {
+        title: 'How much do you know about Dinosaurs?',
+        text: 'Everyone loves Dinosaurs, but how much do we really know about them? Take this quiz to find out if your knowledge is up to snuff.',
+        img: '/resources/img/dinosaurs.jpeg'
+      },
       {
         title: 'Question 1',
         text: 'What was the largest canivorous dinosaur?',
-        img: '',
+        img: '/resources/img/t-rex.jpg',
         options: [
           {
             text: 'Allosaurus',
@@ -36,15 +37,14 @@ const STORE = {
         ],
 
         feedback: {
+          img: 'resources/img/spinosaurus.jpg',
           correct: {
             title: 'CORRECT',
-            text: 'Wow, you really know your stuff!',
-            img: ''
+            text: 'Wow, you really know your stuff!'
           },
           incorrect: {
             title: 'WRONG',
-            text: 'The largest known carnivorous dinosaur is the Spinosaurus, which could grow to be over 50 feet long and weighed in at over 20 tonnes -- three times the weight of a T-rex.',
-            img: ''
+            text: 'The largest known carnivorous dinosaur is the Spinosaurus, which could grow to be over 50 feet long and weighed in at over 20 tonnes -- three times the weight of a T-rex.'
           }
         }
       },
@@ -52,7 +52,7 @@ const STORE = {
       {
         title: 'Question 2',
         text: 'What was the the first dinosaur ever discovered?',
-        img: '',
+        img: '/resources/img/paleontology.jpg',
         options: [
           {
             text: 'Ornithomimus',
@@ -73,15 +73,14 @@ const STORE = {
         ],
 
         feedback: {
+          img: '/resources/img/megalosaurus.gif',
           correct: {
             title: 'CORRECT',
-            text: 'Wow, did you really know that, or did you look it up?',
-            img: ''
+            text: 'Wow, did you really know that, or did you look it up?'
           },
           incorrect: {
             title: 'WRONG',
-            text: 'The Megalosaurus was the first dinosuar discovererd and wasn\'t found until the 1820s',
-            img: ''
+            text: 'The Megalosaurus was the first dinosuar discovererd and wasn\'t found until the 1820s'
           }
         }
       },
@@ -89,7 +88,7 @@ const STORE = {
       {
         title: 'Question 3',
         text: 'What does the word dinosaur mean?',
-        img: '',
+        img: '/resources/img/confused-dinosaur.jpg',
         options: [
           {
             text: 'Terrible Lizzard',
@@ -110,15 +109,14 @@ const STORE = {
         ],
 
         feedback: {
+          img: '/resources/img/terrible.jpg',
           correct: {
             title: 'CORRECT',
-            text: 'Don\'t let it go to your head too much.',
-            img: ''
+            text: 'Don\'t let it go to your head too much.'
           },
           incorrect: {
             title: 'WRONG',
-            text: 'The term “dinosaur” wasn\'t coined until 1842, by English paleontologist Sir Richard Owen; "dinosaur" translates to “terrible reptile.”',
-            img: ''
+            text: 'The term “dinosaur” wasn\'t coined until 1842, by English paleontologist Sir Richard Owen; "dinosaur" translates to “terrible reptile.”'
           }
         }
       },
@@ -126,7 +124,7 @@ const STORE = {
       {
         title: 'Question 4',
         text: 'What were the plates on a stegosaurus\'s back used for?',
-        img: '',
+        img: '/resources/img/stego.jpg',
         options: [
           {
             text: 'Cooling',
@@ -147,15 +145,14 @@ const STORE = {
         ],
 
         feedback: {
+          img: '/resources/img/blood-vessels.jpg',
           correct: {
             title: 'CORRECT',
-            text: 'Are you sure you\'re not cheating? How could you possibly know that?',
-            img: ''
+            text: 'Are you sure you\'re not cheating? How could you possibly know that?'
           },
           incorrect: {
             title: 'WRONG',
-            text: 'Veins in Stegosaurus\' spinal plates suggested that they would not have been used as protective armor; more than likely, they were used to help the dinosaur cool off its massive body.',
-            img: ''
+            text: 'Veins in Stegosaurus\' spinal plates suggested that they would not have been used as protective armor; more than likely, they were used to help the dinosaur cool off its massive body.'
           }
         }
       },
@@ -163,7 +160,7 @@ const STORE = {
       {
         title: 'Question 5',
         text: 'What dinosaur had the largest head?',
-        img: '',
+        img: '/resources/img/big-head.jpeg',
         options: [
           {
             text: 'Diplodocus',
@@ -184,15 +181,14 @@ const STORE = {
         ],
 
         feedback: {
+          img: '/resources/img/triceratops.jpg',
           correct: {
             title: 'CORRECT',
-            text: 'Good job, I guess.',
-            img: ''
+            text: 'Good job, I guess.'
           },
           incorrect: {
             title: 'WRONG',
-            text: 'The Triceratops had the largest shielded skull of any known dinosaur. With a thick bony shield that covered its neck, a Triceratops skull could grow to be as long as 6 and ½ feet long.',
-            img: ''
+            text: 'The Triceratops had the largest shielded skull of any known dinosaur. With a thick bony shield that covered its neck, a Triceratops skull could grow to be as long as 6 and ½ feet long.'
           }
         }
       },
